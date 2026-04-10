1238643c1067d4e7ac8a320f211045cd862afd0e
Antonio-RMz <rodriguezmendezmarcoantonioh@gmail.com>
2026-04-10 15:39:23 -0600

    Punto de respaldo,ya funciona con el server

--- app/config/conexion.py
+++ app/config/conexion.py
@@ -4,7 +4,7 @@ import os
 # Configuración de la base de datos
 DB_CONFIG = {
     'host': '127.0.0.1',
-    'port': 3307,
+    'port': 3306,
     'user': 'api_user',
     'passwd': '123456',
     'db': 'escuelabti',
